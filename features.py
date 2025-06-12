8c3ea023ca97a154dac33e9daf915824600d50a3
melika abadi <69399562+melika-bol@users.noreply.github.com>
2025-06-12 13:06:42 -0700

    feature engineering module

---
+++ features.py
@@ -0,0 +1,8 @@
+def load_data(csv_path):
+  df = pd.read_csv(csv_path)
+  return df
+
+def add_features(df):
+  df['is_small_amount'] = (df['Amount'] <= 2125.87).astype(int)
+  df['log_amount'] = np.log1p(df['Amount'])
+  return df
